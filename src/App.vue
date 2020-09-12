7ce841225f2a00f7d0c93cf5d8820c82c3db63c9
HwuuF7 <914334593@qq.com>
2020-09-12 21:05:34 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+	  APP
+	 <p v-if="age == 18 "> true</p>
+	 <p v-else>false</p>
   </div>
 </template>
 
+<script>
+	
+
+
+export default {
+  name: 'app',
+  data() {
+	  return {
+		  name: 'zhangsan',
+		  age: 88
+	  }
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
